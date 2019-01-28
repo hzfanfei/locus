9071f958337798e8f93d9d0e13df90a3a9957254
fanfei <hzfanfei@corp.netease.com>
2019-01-28 22:01:37 +0800

    locus res

---
+++ locus/locus/LocusRes.h
@@ -0,0 +1,17 @@
+//
+//  LocusRes.h
+//  locus
+//
+//  Created by FanFamily on 2019/1/28.
+//  Copyright © 2019年 niuniu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocusRes : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ locus/locus/LocusRes.m
@@ -0,0 +1,13 @@
+//
+//  LocusRes.m
+//  locus
+//
+//  Created by FanFamily on 2019/1/28.
+//  Copyright © 2019年 niuniu. All rights reserved.
+//
+
+#import "LocusRes.h"
+
+@implementation LocusRes
+
+@end
